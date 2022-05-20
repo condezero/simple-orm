18f3c037255af060b29bd66d2c53dab4906efadc
Carlos Lisandra <carlos.lisandra@toolfactory.net>
2022-05-20 11:02:41 +0200

    added swagger

--- cmd/main.go
+++ cmd/main.go
@@ -7,8 +7,20 @@ import (
 	"simple-orm/pkg/products"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/swagger"
+
+	_ "simple-orm/docs"
 )
 
+// @title Simple ORM API
+// @version 1.0
+// @description This is a PoC SPI.
+// @termsOfService http://swagger.io/terms/
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:3000
+// @BasePath /
+// @schemes http
 func main() {
 	c, err := config.LoadConfig()
 
@@ -21,5 +33,27 @@ func main() {
 
 	products.RegisterRoutes(app, h)
 
+	app.Get("/hc", HealthCheck)
+	app.Get("/swagger/*", swagger.HandlerDefault)
 	app.Listen(c.Port)
+}
+
+// HealthCheck godoc
+// @Summary Show the status of server.
+// @Description get the status of server.
+// @Tags root
+// @Accept */*
+// @Produce json
+// @Success 200 {object} map[string]interface{}
+// @Router /hc [get]
+func HealthCheck(c *fiber.Ctx) error {
+	res := map[string]interface{}{
+		"data": "Server is up and running",
+	}
+
+	if err := c.JSON(res); err != nil {
+		return err
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,21 @@ module simple-orm
 go 1.13
 
 require (
-	github.com/gofiber/fiber v1.14.6
+	github.com/arsmn/fiber-swagger/v2 v2.31.1 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/go-openapi/spec v0.20.6 // indirect
+	github.com/go-openapi/swag v0.21.1 // indirect
+	github.com/gofiber/fiber v1.14.6 // indirect
 	github.com/gofiber/fiber/v2 v2.33.0
+	github.com/gofiber/swagger v0.0.1
+	github.com/klauspost/compress v1.15.4 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/spf13/viper v1.11.0
+	github.com/swaggo/swag v1.8.2
+	github.com/urfave/cli/v2 v2.7.1 // indirect
+	github.com/valyala/fasthttp v1.37.0 // indirect
+	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
+	golang.org/x/sys v0.0.0-20220519141025-dcacdad47464 // indirect
 	gorm.io/driver/postgres v1.3.5
 	gorm.io/gorm v1.23.5
 )
